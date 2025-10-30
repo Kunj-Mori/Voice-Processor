3bfcc1ab24689f8174b8529ee92c0ad0fab129b8
Kunj Mori <kunj.mori.devfrend@gmail.com>
2025-10-30 21:00:45 +0530

    Deploying

--- backend/.render-build.sh
+++ backend/.render-build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "🔧 Installing system dependencies..."
-apt-get update
-apt-get install -y portaudio19-dev gcc libasound-dev
-
 echo "🐍 Installing Python dependencies..."
 pip install --upgrade pip
 pip install wheel setuptools
